8aa1c3b3d6d74588cb00ef833e8c24a8648e3c9a
Rafael Gonçalves <rafaelswitek@gmail.com>
2023-11-26 03:27:36 -0300

    05. Mão na massa: múltiplos usuários

--- views.py
+++ views.py
@@ -1,18 +1,21 @@
 from app import app, bot
-from flask import render_template, request, Response
+from flask import render_template, request, Response,session,flash,redirect, url_for
 import os 
 from helpers import *
 from conta_tokens import *
 from resumidor import criando_resumo
+from models import *
 
 @app.route("/")
 def home():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect(url_for('login'))
     return render_template("index.html")
 
 @app.route("/chat",methods = ['POST'])
 def chat():
     prompt = request.json['msg']
-    nome_do_arquivo = 'historico_ecomart'
+    nome_do_arquivo = session['usuario_logado']
     historico = ''
     if os.path.exists(nome_do_arquivo):
         historico = carrega(nome_do_arquivo)
@@ -42,3 +45,26 @@ def limpar_historico():
     else: 
         print("Não foi possivel remover esse arquivo")
     return {}
+
+@app.route('/login')
+def login():
+    return render_template('login.html', proxima='/')
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if request.form['senha'] == usuario.senha:
+            session['usuario_logado'] = usuario.nickname
+            flash(usuario.nickname + ' logado com sucesso!')
+            return redirect(request.form['proxima'])
+    else:
+        flash('Usuário não logado.')
+        return redirect(url_for('login'))
+    
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect(url_for('login'))
